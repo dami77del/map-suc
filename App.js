16bf02ce2eee11af685367589f197654873f0047
dami77del <ddeligdisch@gmail.com>
2023-03-10 01:29:43 +0100

    add db SQL Lite fixed

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { init } from './db';
 
 import store from './store'
+
+init()
+.then(() => console.log('db initialized success'))
+.catch((err)=>{
+  console.log("Database failed")
+  console.log(err.message)
+})
 
 // navigation
 import MainNavigator from './navigation'
--- store/places.reducer.js
+++ store/places.reducer.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE } from './places.actions'
+import { ADD_PLACE, LOAD_PLACE } from './places.actions'
 import Place from '../Models/Place'
 const initialState = {
     places: [],
@@ -9,15 +9,27 @@ export default (state = initialState, action) => {
         case ADD_PLACE:
             const newPlace = new Place(
                 Date.now(),
-             action.payload.title,
-              action.payload.image,
-              action.payload.lat,
-              action.payload.lng,
-              action.payload.address
-              )
+                action.payload.title,
+                action.payload.image,
+                action.payload.lat,
+                action.payload.lng,
+                action.payload.address
+            )
             return {
                 ...state,
                 places: state.places.concat(newPlace),
+            }
+        case LOAD_PLACE:
+            return {
+                ...state,
+                places: action.places.map(item => new Place(
+                    item.id.toString(),
+                    item.title,
+                    item.image,
+                    item.lat,
+                    item.lng,
+                    item.address,
+                ))
             }
         default:
             return state
